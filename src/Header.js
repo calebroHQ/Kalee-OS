bf65fa4a83a1b3378446aa044223b29beba11c31
calebro <calebro@localhost.localdomain>
2021-06-28 17:30:40 +0300

    final update

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ function Header() {
         <div className='header'>
              <img
                     className="header__icon"
-                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                    src="https://www.canva.com/design/DAEhlXCKbGE/v6gnkxOU-JAV9qqb38A3zQ/view?utm_content=DAEhlXCKbGE&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
                     alt=""
                 /> 
             <div className='header__center'>
